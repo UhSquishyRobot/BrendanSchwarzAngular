66eebd7ee004f01f1c6a30610adc2c9606b2e8af
brendan schwarz <bschwarz890@gmail.com>
2020-05-01 12:43:56 -0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { VideoPageComponent } from './video-page/video-page.component';
+import { MusicPageComponent } from './music-page/music-page.component';
+import { WritingPageComponent } from './writing-page/writing-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: 'home', component: HomePageComponent },
+	{ path: 'video', component: VideoPageComponent },
+	{ path: 'music', component: MusicPageComponent },
+	{ path: 'writing', component: WritingPageComponent },
+	{ path: 'contact', component: ContactPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
